5e89baadbd210eb34fcd7f157bfe42cf29daa99a
Justin Munn <jumunn@microsoft.com>
2020-04-23 04:01:57 -0700

    Cleaned up using statements and comments

--- Lesson01_UrlShorteners/UrlShortener/UrlShortener/Program.cs
+++ Lesson01_UrlShorteners/UrlShortener/UrlShortener/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace UrlShortener
 {
@@ -10,14 +7,10 @@ namespace UrlShortener
     {
         const string _base62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-        // LpuPe81bc2w => 
-        // 18327995462734721974
-        // 18327995462734721024
-        // my answer 17625286683235144704‬
-
         // 1. For our first problem, write a function that converts a given base-62 string
         //    into an integer. Only a single string will be provided, and it will be up to
         //    11 characters in length.  
+        // LpuPe81bc2w => 18327995462734721974
         public static ulong ToBase10(string videoId)
         {
             int videoIdLength = videoId.Length - 1;
@@ -39,6 +32,7 @@ namespace UrlShortener
         // 2. Write a function that does the opposite of the previous one. 
         // That is, it encodes a base 10 number into base 62
         // producing a string that represents the same number.
+        // 18327995462734721974 => LpuPe81bc2w
         public static string ToBase62(ulong number)
         {
             StringBuilder sb = new StringBuilder();
